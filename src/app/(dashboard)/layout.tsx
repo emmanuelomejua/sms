9749cc92fe0068390eec74072a29714db262db2d
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-10-19 16:56:23 +0000

    navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,20 +1,17 @@
-import type { Metadata } from "next";
+// import Menu from "@/components/Menu";
+// import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 
-export const metadata: Metadata = {
-    title: "School Management System",
-    description: "School Management System - Dashboard",
-  };
-  
-  export default function DashboardLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-        <div className="">
-            {children}
-            Whoo
-        </div>
-    );
-  }
-  
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w[14%] bg-white">l</div>
+      <div className="w-[86%] md:[92%] lg:w-[84%] xl:w[86%] bg-slate-800">r</div>
+    </div>
+  );
+}
