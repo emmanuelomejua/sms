099a885799005f9f03461fb7482de35b4c78f3c8
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-10-20 01:29:35 +0000

    navbar completed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,5 @@
-// import Menu from "@/components/Menu";
-// import Navbar from "@/components/Navbar";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,8 +10,17 @@ export default function DashboardLayout({
 }>) {
   return (
     <div className="h-screen flex">
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w[14%] bg-white">l</div>
-      <div className="w-[86%] md:[92%] lg:w-[84%] xl:w[86%] bg-slate-800">r</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w[14%] bg-white p-2">
+        <Link href='/' className="flex items-center justify-center gap-2 lg:justify-start">
+          <Image src='/logo.png' alt="Logo" width={32} height={32}/>
+          <span className="hidden lg:block">VAULT SMS</span>
+        </Link>
+        <Menu/>
+      </div>
+      <div className="w-[86%] md:[92%] lg:w-[84%] xl:w[86%] bg-[#f7f8fa] overflow-y-scroll flex flex-col">
+        <Navbar/>
+        {children}
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between p-4'>
+      <div className="hidden md:flex rounded-full gap-2 text-xs px-2 py-1 ring-[1.5px] ring-gray-300">
+        <Image src='/search.png' alt='' width={14} height={10} />
+        <input type="text" className="outline-none bg-transparent w-[200px] p-2" placeholder='Search...'/>
+      </div>
+
+      <div className="flex items-center gap-4 justify-end w-full">
+        <div className="rounded-full bg-white w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src='/message.png' alt='' width={20} height={20} />
+        </div>
+
+        <div className="rounded-full bg-white w-7 h-7 flex items-center justify-center cursor-pointer relative">
+          <Image src='/announcement.png' alt='' width={20} height={20} />
+          <span className='absolute bg-purple-500 h-5 w-5 flex items-center justify-center -top-3 -right-3 rounded-full text-xs text-white'>4</span>
+        </div>
+
+        <div className="flex flex-col">
+          <span className="leading-3 text-xs font-medium">John David</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+
+        <Image src='/avatar.png' alt='' width={36} height={36} className='rounded-full' />
+      </div>
+    </div>
   )
 }
 
