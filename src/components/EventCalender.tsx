171b9404499d9511a42c372616f9a62aa6fec746
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-10-21 03:55:37 +0000

    finance chart added

---
+++ src/components/EventCalender.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const EventCalender = () => {
+  return (
+    <div className='bg-white p-4 rounded-md'>EventCalender</div>
+  )
+}
+
+export default EventCalender;
