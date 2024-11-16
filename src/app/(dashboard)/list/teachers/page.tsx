3be974c2b636be58f5938b8ae61548c3d2eedc49
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-11-16 16:14:49 +0000

    menu items

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -61,7 +61,7 @@ const TeacherList = () => {
   const renderRow = (item: Teacher) => (
     <tr
     key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
   >
     <td className="flex items-center gap-4 p-4">
       <Image
@@ -84,12 +84,12 @@ const TeacherList = () => {
     <td>
       <div className="flex items-center gap-2">
         <Link href={`/list/teachers/${item.id}`}>
-          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
             <Image src="/view.png" alt="" width={16} height={16} />
           </button>
         </Link>
         {role === "admin" && (
-          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+          <button className="w-7 h-7 flex items-center justify-center rounded-full bg-vPurple">
             <Image src="/delete.png" alt="" width={16} height={16} />
           </button>
         )}
